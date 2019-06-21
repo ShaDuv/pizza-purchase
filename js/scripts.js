fb99a4b82481250bbf8987e67fc8a7b3b38ca130
Shawna Duvall <shawnaduvall@gmail.com@>
2019-06-21 10:55:45 -0700

    required commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,33 @@
 //-------------Bussiness Logic----------//
-const toppings = {
-  cheese: 0.75
-  pepperoni:  0.75
-  chicken:  2.00
-  bacon:  1.00
-  peppers: 0.25
-  olives: 0.50
-}
+// -- Pizza Constructor -- //
+function PizzaConst(initialPrice, toppings, size) {
+  this.base = base,
+  this.addons = []
+};
 
-const size = {
-  small:  1.00
-  medium: 2.00
+const addons = {
+  cheese: 0.75,
+  pepperoni: 0.75,
+  chicken:  2.00,
+  bacon:  1.00,
+  peppers: 0.25,
+  olives: 0.50,
+  small:  1.00,
+  medium: 2.00,
   large:  3.00
-}
+};
 
-const baseprice = 4.00
+const baseprice = 4.00;
 
-    // -- Pizza Constructor -- //
-function PizzaConst(baseprice, toppings, size) {
-  this.base = base;
-  this.toppings = toppings;
-  this.size = size;
 
-  
-}
 //-------------User Logic--------------//
+$(document).ready(function () {
+  $('#magic-pizza-button').submit(function () {
+    event.preventDefault();
+    var userAddons = $(input:checked).name(){
+      console.log(userAddons);
+    };
+
+  });
+
+});
