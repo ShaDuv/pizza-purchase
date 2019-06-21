fd274d5ee715dbc4ef1f4192f91d0febc4399ff7
Shawna Duvall <shawnaduvall@gmail.com@>
2019-06-21 15:31:57 -0700

    add functionality to remove checked items from price

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,27 @@
 //-------------Bussiness Logic----------//
 // -- Pizza Constructor -- //
-function PizzaConst(initialPrice, checked) {
+function PizzaConst() {
   this.base = 4.00;
   this.addonsArray = [];
-  this.total = this.base;
+  this.total = 0;
+  this.size = 'small';
 };
 
 PizzaConst.prototype.addToPizza = function(addons) {
   this.addonsArray.push(addons);
 };
 
+PizzaConst.prototype.setSize = function(size) {
+  this.size = size;
+};
+
+
 PizzaConst.prototype.calculatePrice = function() {
+  this.total = this.base;
   for (i = 0; i < this.addonsArray.length; i++) {
     this.total = this.total + addons[this.addonsArray[i]];
   }
+  this.total = this.total + sizes[this.size];
 };
 const addons = {
   cheese: 0.75,
@@ -21,22 +29,33 @@ const addons = {
   chicken: 2.00,
   bacon: 1.00,
   peppers: 0.25,
-  olives: 0.50,
+  olives: 0.50
+};
+
+const sizes = {
   small: 1.00,
   medium: 2.00,
   large: 3.00
 };
+
 var newPizza = new PizzaConst();
-// var checked = [];
+
 //-------------User Logic--------------//
 $(document).ready(function() {
-  $('#magic-pizza-button').click(function() {
+    $('form').click(function() {
+      newPizza.addonsArray = [];
       $("input[name='addons']:checked").each(function() {
         newPizza.addToPizza($(this).val());
+      });
+      $("input[name='sizes']:checked").each(function() {
+        newPizza.setSize($(this).val());
       });
       newPizza.calculatePrice();
       console.log(newPizza.total);
     });
-    addons.forEach(function (x) {
-        $('.form-group).html('Welcome ' + x.name + '!'); 
-}); //closes doc ready
+
+    // for (i = 0; i < addons; i++) {
+    //   $('.toppings').append('<input type="checkbox" name="addons" value="' + addon[i] + '">' + addon[i]);
+    // };
+
+  }); //closes doc ready
