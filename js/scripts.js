4dc4c97d3a9155f36b5c295c8db59b353cc2d0ac
Shawna Duvall <shawnaduvall@gmail.com@>
2019-06-21 09:58:11 -0700

    beging the business logic in JS and the start of the  HTML form.

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+//-------------Bussiness Logic----------//
+const toppings = {
+  cheese: 0.75
+  pepperoni:  0.75
+  chicken:  2.00
+  bacon:  1.00
+  peppers: 0.25
+  olives: 0.50
+}
+
+const size = {
+  small:  1.00
+  medium: 2.00
+  large:  3.00
+}
+
+const baseprice = 4.00
+
+    // -- Pizza Constructor -- //
+function PizzaConst(baseprice, toppings, size) {
+  this.base = base;
+  this.toppings = toppings;
+  this.size = size;
+
+  
+}
+//-------------User Logic--------------//
