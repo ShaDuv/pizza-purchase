b5c4c274bede6e7bae2dd696d97cc30ebdff6b59
Shawna Duvall <shawnaduvall@gmail.com@>
2019-06-21 17:06:12 -0700

    adds display confirmation and user name

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,12 @@ function PizzaConst() {
   this.total = 0;
   this.size = 'small';
 };
+function ClientConst(firstN, lastN, phone, address) {
+  this.firstName = firstN;
+  this.lastName = lastN;
+  this.phone = phone;
+  this.address= address;
+};
 
 PizzaConst.prototype.addToPizza = function(addons) {
   this.addonsArray.push(addons);
@@ -29,7 +35,8 @@ const addons = {
   chicken: 2.00,
   bacon: 1.00,
   peppers: 0.25,
-  olives: 0.50
+  olives: 0.50,
+  mushrooms: 0.25
 };
 
 const sizes = {
@@ -42,6 +49,11 @@ var newPizza = new PizzaConst();
 
 //-------------User Logic--------------//
 $(document).ready(function() {
+    // for (i = 0; i < addons; i++) {
+    //   var toppingBtns = $('<input type="checkbox" name="addons" value="' + addons[i] + '">' + addons[i]);
+    //   ('<input type="radio" name="rbtnCount" />');
+    //   toppingBtns.appendTo('#toppings');
+    // };
     $('form').click(function() {
       newPizza.addonsArray = [];
       $("input[name='addons']:checked").each(function() {
@@ -51,11 +63,19 @@ $(document).ready(function() {
         newPizza.setSize($(this).val());
       });
       newPizza.calculatePrice();
-      console.log(newPizza.total);
     });
 
-    // for (i = 0; i < addons; i++) {
-    //   $('.toppings').append('<input type="checkbox" name="addons" value="' + addon[i] + '">' + addon[i]);
-    // };
+    $('#magic-pizza-button').click(function(event) {
+      var firstN = $("input#first").val()
+      var lastN = $("input#last").val()
+      var phone = $("input#phone").val()
+      var address = $("input#address").val()
+      var newClient = new ClientConst(firstN, lastN, phone, address);
+      $(".output").append("Your order has been placed. Your pizza will be delieverd to " + firstN + " " + lastN + ' and your total will be ' + newPizza.total);
+      console.log(firstN)
+      event.preventDefault();
+
+    });
+
 
   }); //closes doc ready
